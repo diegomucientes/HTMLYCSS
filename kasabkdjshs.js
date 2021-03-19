35d76d196c2bc98484bacce51b4677e3da6b25b2
diegomucientes <diego.mucientes.medina@gmail.com>
2021-03-18 21:37:04 -0300

    CatCommit

---
+++ kasabkdjshs.js
@@ -0,0 +1,6 @@
+function printUpTo(x) {
+  // your code here
+}
+printUpTo(1000); // debería imprimir todos los enteros de 1 to 1000
+y = printUpTo(-10); // debería imprimir false
+console.log(y); // debería imprimir false
